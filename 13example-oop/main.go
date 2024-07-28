dbb5aeb528946da6e16547e0b33b0813ab56dbc6
Sergio Lima <sergiosouzalima@gmail.com>
2024-07-27 22:28:18 -0300

    adding a new object

--- 13example-oop/main.go
+++ 13example-oop/main.go
@@ -4,23 +4,26 @@ import "fmt"
 
 // Define a struct chamada Person
 type Person struct {
-    Name string
-    Age  int
+	Name string
+	Age  int
 }
 
 // Define um método para a struct Person
 // Este método retorna uma string que descreve a pessoa
 func (p Person) Greet() string {
-    return fmt.Sprintf("Hello, my name is %s and I am %d years old.", p.Name, p.Age)
+	return fmt.Sprintf("Hello, my name is %s and I am %d years old.", p.Name, p.Age)
 }
 
 func main() {
-    // Cria uma instância da struct Person
-    person := Person{Name: "Alice", Age: 30}
+	// Cria uma instância da struct Person
+	person := Person{Name: "Alice", Age: 30}
+	person2 := Person{Name: "John", Age: 57}
 
-    // Chama o método Greet da instância person
-    greeting := person.Greet()
+	// Chama o método Greet da instância person
+	greeting := person.Greet()
+	// Imprime a saudação
+	fmt.Println(greeting)
 
-    // Imprime a saudação
-    fmt.Println(greeting)
+	// Imprime a saudação
+	fmt.Println(person2.Greet())
 }
